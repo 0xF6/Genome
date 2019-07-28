3317cd8894d6e6f53eca87e5e9fc229f837464f3
Yuuki Wesp <ls-micro@ya.ru>
2019-07-28 03:27:52 +0300

    add codon

---
+++ src/System.Molecular/Extra/Codon.cs
@@ -0,0 +1,15 @@
+﻿namespace System.Molecular.Extra
+{
+    public sealed class Codon
+    {
+        public bool IsProteinInitiator()
+        {
+            // if AUG codon
+        }
+
+        public bool IsStop()
+        {
+            // if UAG, UGA, UAA
+        }
+    }
+}
