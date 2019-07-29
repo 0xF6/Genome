18d15be071223baedca4ef46507a133ecf9f0422
Yuuki Wesp <ls-micro@ya.ru>
2019-07-29 06:24:50 +0300

    upd test

--- test/System.Molecular.Test/TranscribeTest.cs
+++ test/System.Molecular.Test/TranscribeTest.cs
@@ -1,5 +1,6 @@
 namespace Tests
 {
+    using System.Linq;
     using System.Molecular;
     using NUnit.Framework;
 
@@ -11,6 +12,7 @@ namespace Tests
             var code = "TAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCCTAACCC";
             var dna = RNA.Transcribe(code);
             Assert.AreEqual(code.Length / 3, dna.Codons.Length);
+            Assert.True(dna.Codons.First().IsStop());
         }
     }
 }
