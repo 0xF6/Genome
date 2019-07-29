748b88fd083cb763704ec34758c6dad9c85f9d20
Yuuki Wesp <ls-micro@ya.ru>
2019-07-29 06:25:21 +0300

    add byte to codon converter

---
+++ src/byte2dnk/Host.cs
@@ -0,0 +1,38 @@
+﻿namespace byte2dnk
+{
+    using System;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using Algorithm;
+    using Pastel;
+    using static Log;
+    internal static class Host
+    {
+        public static void Main(string[] args)
+        {
+            //if(!args.Any())
+            //    Error($"No file specified.", 1);
+            StringToCodons();
+        }
+
+
+        public static void StringToCodons()
+        {
+            var str = "Влад Гей";
+
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Info($"String: '{str.Pastel(Color.Gray)}', Length: {str.Length}");
+            var asw = HexCodon.Encode(Encoding.UTF8.GetBytes(str)).Select(x =>
+                $"{x.Acid.Symbol.ToString().Pastel(getColorByAminoAcidsState(x.Acid.State))}");
+            Info($"acid map: {string.Join(" ", asw)}");
+            Info($"");
+            Info($"Detail table:");
+
+            foreach (var codon in HexCodon.Encode(Encoding.UTF8.GetBytes(str)))
+                Info($"\t{codon.Value.Pastel(getColorByAminoAcidsState(codon.Acid.State))} {codon.Acid.FullName}/{codon.Acid.Symbol}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ src/byte2dnk/Log.cs
@@ -0,0 +1,38 @@
+﻿namespace byte2dnk
+{
+    using System;
+    using System.Drawing;
+    using System.Molecular.Acid;
+    using Pastel;
+    using static System.Console;
+
+    internal static class Log
+    {
+        public static void Error(string msg, int? exitCode)
+        {
+            WriteLine($"[{"b2d".Pastel(Color.Coral)}][{"ERR".Pastel(Color.Red)}]: {msg}");
+            if(exitCode is null)
+                return;
+            Environment.Exit(exitCode.Value);
+        }
+        public static void Warn(string msg)
+        {
+            WriteLine($"[{"b2d".Pastel(Color.Coral)}][{"WRN".Pastel(Color.Orange)}]: {msg}");
+        }
+        public static void Info(string msg)
+        {
+            WriteLine($"[{"b2d".Pastel(Color.Coral)}]: {msg}");
+        }
+
+        public static Color getColorByAminoAcidsState(AAS state) =>
+            state switch
+            {
+                AAS.Acidic      => Color.MediumPurple,
+                AAS.Basic       => Color.CornflowerBlue,
+                AAS.NonPolar    => Color.Yellow,
+                AAS.Polar       => Color.MediumSeaGreen,
+                AAS.Terminator  => Color.DarkRed,
+                _               => Color.White,
+            };
+    }
+}
