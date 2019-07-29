6115e6c12bbeb018ac27d348e165547bfee4b01a
Yuuki Wesp <ls-micro@ya.ru>
2019-07-29 03:29:34 +0300

    add converter DNA to RNA and Reverse

--- src/System.Molecular/Acid/NucleicAcid.cs
+++ src/System.Molecular/Acid/NucleicAcid.cs
@@ -1,10 +1,28 @@
 ﻿namespace System.Molecular.Acid
 {
     using Collections.Generic;
+    using Diagnostics;
     using Extra;
+    using JetBrains.Annotations;
+    using Linq;
 
+    [PublicAPI, DebuggerDisplay("{ToString()}")]
     public class NucleicAcid
     {
-        public IEnumerable<Codon> Codons { get; set; }
+        public Codon[] Codons { get; set; }
+
+        public NucleicAcid(IEnumerable<Codon> codons) => Codons = codons.ToArray();
+
+        #region Overrides of Object
+
+        public override string ToString() 
+            => string.Join(" ", Codons.Select(x => x.ToString()));
+
+        #endregion
+
+        internal static string RNA_TO_DNA(string code) => code.ToUpperInvariant()
+            .Replace("U", "T").Replace("\r", "").Replace("\n", "");
+        internal static string DNA_TO_RNA(string code) => code.ToUpperInvariant()
+            .Replace("T", "U").Replace("\r", "").Replace("\n", "");
     }
 }
